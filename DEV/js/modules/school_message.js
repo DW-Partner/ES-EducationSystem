d3884a2666ae2d5f7137b9edd76cf942f216ce23
yuwenzhuifeng <yuwenzhuifeng@qq.com>
2017-10-08 23:40:11 +0800

    校区-消息页面

---
+++ DEV/js/modules/school_message.js
@@ -0,0 +1,10 @@
+	require('./school_message.css');//引入css文件
+	$.mainBox.on('click', 'p', ()=>{
+		//alert(5678);
+	})
+
+	//distory方法，挂载与$上
+	$.distory = ()=>{
+		$.mainBox.off();//可以不设置，在CMD文件中有统一off调用
+		console.log('distory!')
+	}
