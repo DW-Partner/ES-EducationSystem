606732eba2847683aafc1a83ce5fa42b9b152b9f
yuwenzhuifeng <yuwenzhuifeng@qq.com>
2017-10-09 18:58:37 +0800

    引入Md5加密js

--- DEV/js/modules/login.js
+++
@@ -1,8 +0,0 @@
-	require('./login.css');//引入css文件
-
-
-	//distory方法，挂载与$上
-	$.distory = ()=>{
-		$.mainBox.off();//可以不设置，在CMD文件中有统一off调用
-		console.log('distory!')
-	}
---
+++ DEV/js/modules/zone_login.js
@@ -0,0 +1,2 @@
+	require('./zone_login.css');//引入css文件
+	import hex_md5 from '../kit/md5.js';
