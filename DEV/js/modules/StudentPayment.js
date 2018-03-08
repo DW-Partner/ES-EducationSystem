b2c4d8ce23e0e980ec7621d0235f99fc1b555bed
yuwenzhuifeng <yuwenzhuifeng@qq.com>
2018-03-08 11:36:12 +0800

    notes

--- DEV/js/modules/StudentPayment.js
+++ DEV/js/modules/StudentPayment.js
@@ -15,9 +15,9 @@ let getStudentPaySum = ()=>{
         dataType: "json",
         url: '/pss/getStudentPaySum',
         data: {
-	        code: $('#zone_code').val(),
-	        zoneid: $('#zone_zoneid').val(),
-	        sid: sid
+          code: $('#zone_code').val(),
+          zoneid: $('#zone_zoneid').val(),
+          sid: sid
         },
         success: (res)=>{
             if( res.errcode != 0 ){
@@ -72,36 +72,37 @@ getStudentPayRecord();
 
 
 $.mainBox.on('click', '#submit', function(){
-	const sub_data = $.form.get({
-        error_text: 'placeholder',//存放错误文案的属性名
-	});
-	if( !sub_data ){
-		return;
-	}
+    const sub_data = $.form.get({
+          error_text: 'placeholder',//存放错误文案的属性名
+    });
+    if( !sub_data ){
+      return;
+    }
 
     let ajaxData = {
         code: $('#zone_code').val(),
         zoneid: $('#zone_zoneid').val(),
         sid: sid,
-		money: +sub_data.money,
-		lessons: +sub_data.lessons
+        money: +sub_data.money,
+        lessons: +sub_data.lessons,
+        notes: sub_data.notes
     }
 
-	$.form.submit({
-		url: '/pss/studentPayment',
-		data: ajaxData,
-		success: (res) => {
-			if( res.errcode != 0 ){
-         		$.dialogFull.Tips( res.errmsg );
-				return;
-			}
-        	$.dialogFull.Tips( "提交成功！" );
-         	$.ajaxGetHtml({
-         		url: res.data.url
-         	})
-		},
-        error: function(){
-        	$.dialogFull.Tips( "网络错误，请稍后重试" );
+    $.form.submit({
+      url: '/pss/studentPayment',
+      data: ajaxData,
+      success: (res) => {
+        if( res.errcode != 0 ){
+              $.dialogFull.Tips( res.errmsg );
+          return;
         }
-	});
+            $.dialogFull.Tips( "提交成功！" );
+            $.ajaxGetHtml({
+              url: res.data.url
+            })
+      },
+          error: function(){
+            $.dialogFull.Tips( "网络错误，请稍后重试" );
+          }
+    });
 })
