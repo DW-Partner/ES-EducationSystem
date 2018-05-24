e2e725ae507c0e7770edf823473df5f7c5365949
yuwenzhuifeng <yuwenzhuifeng@qq.com>
2018-05-24 10:33:41 +0800

    comment

--- DEV/js/modules/StudentArchive.js
+++ DEV/js/modules/StudentArchive.js
@@ -247,7 +247,7 @@ $.mainBox.on('click', '.class_list .info', function(){
 		        		li += `<li><img src="${value}"></li>`
 		        	}
 		        }
-		        $( '.report' ).html( li ).after( `<p class="commnet">${res.data.commnet}</p>` );
+		        $( '.report' ).html( li ).after( `<p class="commnet">${res.data.comment}</p>` );
 		        
 		        self.addClass( 'click' ).siblings( '.click' ).removeClass( 'click' );
 		    },
